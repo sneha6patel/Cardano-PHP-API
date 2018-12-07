48930aa3991fed9e81fc129524dcf8d8632b6aa6
dogpatchmedia <jon@dogpatchmedia.com>
2018-12-07 09:08:33 -0800

    Add files via upload
    Cardano PHP API Functions:
    
    GET - GET INFO ABOUT NODE

---
+++ getInfo.php
@@ -0,0 +1,56 @@
+<?php
+
+// GET CURRENT NODE INFO
+// Function  : Delete a wallet and all its associated accounts.
+// Parameters: 
+//
+//				NONE
+// Returns: 
+//
+//				CURRENT NODE STATUS
+//
+
+
+function cardano_get_info() {
+
+		// SETUP
+		$host 		= "https://127.0.0.1";
+		$port 		= "8090";
+        $end_point	= "/api/v1/node-info";
+
+        // CARDANO CLIENT CERTIFICATE
+        $cert_path	= "/var/www/1234ada/cardano-sl/state-wallet-mainnet/tls/client/client.pem";
+
+        // INIT CURL
+        $curl = curl_init();
+       
+       	// OPTIONS
+        curl_setopt($curl, CURLOPT_URL, $host.':'.$port.$end_point);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($curl, CURLOPT_SSLCERT, $cert_path);
+
+        
+        // LET'S GET CURLY
+        $data       = curl_exec($curl);
+        $httpCode   = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+ 
+        // ERRORS
+        if(curl_exec($curl) === false)
+        {
+            throw new Exception('Curl error: ' . curl_error($curl));
+        }
+
+
+        // BYE FELICIA
+        curl_close($curl);
+
+        // GIMME ALL YOUR DATA
+        return $data;
+}
+
